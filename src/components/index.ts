354781892db16f2cf427d7b55bdf4edcae0097fb
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-01-27 09:03:18 +0200

    clean import and export

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as Card } from './Card';
+export { default as Cards } from './Cards';
+export { default as GameBoard } from './GameBoard';
