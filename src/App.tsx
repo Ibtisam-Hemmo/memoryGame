89c524d2085ab8c85339558042341a5c84f961e8
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-02 14:32:11 +0200

    render different types of cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { useGameContext } from './context/gameContext';
+import { Card } from './types/gameType';
 
 const App = () => {
 
-  const { cards, moves, gameStatus, flipCard, resetGame } = useGameContext();
-
+  const { gameState, flipCard, resetGame } = useGameContext();
+  const renderCardContent = (card: Card) => {
+    if (card.isFlipped || card.isMatched) {
+      return card.type === "images" ? (
+        <img
+          src={card.content}
+          alt="Card"
+          style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        />
+      ) : (
+        card.content
+      );
+    }
+    return "Not-Flipped";
+  };
 
   return (
     <div>
       <h1>Memory Game</h1>
-      <p>Moves: {moves}</p>
-      <p>Game Status: {gameStatus}</p>
+      <p>Moves: {gameState.moves}</p>
+      <p>Game Status: {gameState.gameStatus}</p>
+      <div>Timer: {gameState.countDownTimer}s</div>
       <button onClick={resetGame}>Reset Game</button>
       <div style={{ display: "flex", flexWrap: "wrap", gap: '5px' }}>
-        {cards.map((card) => (
+        {gameState.cards.map((card) => (
           <div
             key={card.id}
             onClick={() => !card.isMatched && !card.isFlipped && flipCard(card.id)}
@@ -24,7 +39,7 @@ const App = () => {
               textAlign: 'center'
             }}
           >
-            {card.isFlipped || card.isMatched ? card.content : 'Not-Flipped'}
+            {renderCardContent(card)}
           </div>
         ))}
       </div>
