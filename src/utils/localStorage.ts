a74deaf146d853c5c19e657693901c345fc7027f
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-02 14:34:25 +0200

    Fix local storage to help with migration and other versions

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,26 +1,45 @@
-import { Card } from "../types/gameType";
+import { GameState } from "../types/gameType";
+import { generateCards, getGridSize } from "./generateCards";
 
-export const saveGameToLocalStorage = (
-    cards: Card[],
-    flippedCards: number[],
-    moves: number,
-    gameStatus: "in-progress" | "completed"
-) => {
+export const saveGameToLocalStorage = (gameState: GameState) => {
     const gameData = {
-        cards,
-        flippedCards,
-        moves,
-        gameStatus
+        version: GAME_VERSION,
+        state: gameState
     };
     localStorage.setItem('memoryGame', JSON.stringify(gameData));
+}
+
+const GAME_VERSION = "1.1";
+export const getGameFromLocalStorage = (): GameState | null => {
+    const savedData = localStorage.getItem("memoryGame");
+    if (!savedData) return null;
+
+    const parsedData = JSON.parse(savedData);
+    console.log('parsedData: ', parsedData);
+
+    if (!parsedData.version || parsedData.version !== GAME_VERSION) {
+        console.log(`Migrating from v ${parsedData.version || "unknown"} to ${GAME_VERSION}`);
+
+        const migratedData = {
+            version: GAME_VERSION,
+            state: {
+                ...parsedData,
+                level: parsedData.level || "easy",
+                theme: parsedData.theme || "letters",
+                cards: generateCards(parsedData.theme || "letters", parsedData.level || "easy"),
+                gridSize: parsedData.gridSize || getGridSize(parsedData.level || "easy"),
+                countDownTimer: parsedData.countDownTimer || 150,
+            },
+        };
+
+        console.log('After migration: ', migratedData);
+
+        saveGameToLocalStorage(migratedData.state);
+
+        return migratedData.state;
+    }
+
+    return parsedData.state;
 };
 
-export const getGameFromLocalStorage = (): {
-    cards: Card[];
-    flippedCards: number[];
-    moves: number;
-    gameStatus: "in-progress" | "completed"
-} | null => {
-    const savedData = localStorage.getItem('memoryGame');
-    return savedData ? JSON.parse(savedData) : null;
-};
+
