cc5aa531c36087c81567a1c0d252a4c1d2f5756f
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-02 14:35:24 +0200

    fix initialize game

--- src/utils/initializeGame.ts
+++ src/utils/initializeGame.ts
@@ -1,43 +1,28 @@
-import { cards } from "../assets/Cards";
-import { Card } from "../types/gameType";
+import { Card, GameState, Levels, Themes } from "../types/gameType";
+import { generateCards, getGridSize, getTimerByLevel } from "./generateCards";
 import { getGameFromLocalStorage } from "./localStorage";
-import { shuffle } from "./shuffleFunction";
 
-export const initializeGame = (): {
-    cards: Card[];
-    flippedCards: number[];
-    moves: number;
-    gameStatus: "in-progress" | "completed";
-} => {
+export const initializeGame = (theme: Themes, level: Levels): GameState => {
     const savedGame = getGameFromLocalStorage();
+    console.log('savedGame: ', savedGame);
+
     if (savedGame) {
-        return savedGame;
+        return {
+            ...savedGame,
+            cards: savedGame.cards.map((card: Card) =>
+                card.isMatched ? card : { ...card, isFlipped: false }
+            ),
+            flippedCards: [],
+        };
     }
     return {
-        cards: shuffle(cards),
+        cards: generateCards(theme, level),
         flippedCards: [],
         moves: 0,
-        gameStatus: "in-progress",
+        gameStatus: "inProgress",
+        theme,
+        level,
+        gridSize: getGridSize(level),
+        countDownTimer: getTimerByLevel(level)
     };
 };
-
-// export const initializeGame = (
-//     setCards: React.Dispatch<React.SetStateAction<Card[]>>,
-//     setFlippedCards: React.Dispatch<React.SetStateAction<number[]>>,
-//     setMoves: React.Dispatch<React.SetStateAction<number>>,
-//     setGameStatus: React.Dispatch<React.SetStateAction<'in-progress' | 'completed'>>
-
-// ) => {
-//     const savedGame = getGameFromLocalStorage();
-//     if (savedGame) {
-//         console.log('there is savedGame: ', savedGame);
-
-//         const { cards, flippedCards, moves, gameStatus } = savedGame;
-//         setCards(cards);
-//         setFlippedCards(flippedCards);
-//         setMoves(moves);
-//         setGameStatus(gameStatus || 'in-progress');
-//     } else {
-//         setCards(shuffle(cards))
-//     }
-// };
