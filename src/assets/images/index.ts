5f87e52746cbfe87699b3390b68dc84673ed114f
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-02 14:32:26 +0200

    clean import & export

---
+++ src/assets/images/index.ts
@@ -0,0 +1,13 @@
+import apple from "./appleCharacter.jpeg";
+import banana from "./bananaCharacter.jpg";
+import grape from "./grapeCharacter.jpg";
+import strawberry from "./StrawberryCharacter.jpg";
+import orange from "./OrangeCharacter.jpg";
+
+export {
+    apple,
+    banana,
+    grape,
+    strawberry,
+    orange
+};
