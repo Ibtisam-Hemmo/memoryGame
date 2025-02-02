f754f1f953617f2d350b57e16bb9d288c366fc16
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-02 14:32:54 +0200

    add more types

--- src/types/gameType.ts
+++ src/types/gameType.ts
@@ -1,23 +1,46 @@
 type Card = {
     id: number,
+    type: string,
     content: string,
+    lastFlipTime:number,
     isFlipped: boolean,
     isMatched: boolean,
 }
 
-type GameStatus = "in-progress" | "completed"
+type GameStatus = "inProgress" | "completed" | "failed"
+
+type Themes = "letters" | "images" | "icons"
+
+type Levels = "easy" | "medium" | "hard"
+
+type GridSize = {
+    rows: number;
+    columns: number;
+};
+
+interface GameState {
+    cards: Card[];
+    flippedCards: number[];
+    moves: number;
+    gameStatus: GameStatus;
+    theme:Themes;
+    level:Levels;
+    gridSize:GridSize;
+    countDownTimer: number;
+}
 
 type GameContextType = {
-    cards: Card[],
-    moves: number,
-    gameStatus: GameStatus,
+    gameState: GameState,
     flipCard: (id: number) => void,
     resetGame: () => void,
-    };
+};
 
 
 export type {
     Card,
+    GameState,
+    Themes,
+    Levels,
     GameStatus,
     GameContextType
 }
