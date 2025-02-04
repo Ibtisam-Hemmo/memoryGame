f5c8f706031504f609c16c90a4028dabb558867a
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-04 11:51:27 +0200

    ceate this compponent to render settings

---
+++ src/components/ChoiceGroup.tsx
@@ -0,0 +1,26 @@
+import styles from '../pages/first.module.scss';
+import { ChoiceGroup as ChoiceGroupType } from '../types/componentType';
+
+const ChoiceGroup = <T extends string>({
+    title,
+    choices,
+    selected,
+    onSelect }: ChoiceGroupType<T>) => (
+
+    <div className={styles.choiceContainer}>
+        <span className={styles.mainChoice}>{title}</span>
+        <div className={styles.choicesGroup}>
+            {choices.map((choice) => (
+                <span
+                    key={choice}
+                    className={`${styles.choice} ${selected === choice ? styles.active : ""}`}
+                    onClick={() => onSelect(choice)}
+                >
+                    {choice}
+                </span>
+            ))}
+        </div>
+    </div>
+);
+
+export default ChoiceGroup;
