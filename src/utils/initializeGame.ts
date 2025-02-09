9dc4ec228f65c05895d166aff5a5a62b47359efe
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-09 17:09:17 +0200

    fix the returned saved game

--- src/utils/initializeGame.ts
+++ src/utils/initializeGame.ts
@@ -6,17 +6,18 @@ export const initializeGame = (theme: Themes, level: Levels): GameState => {
     const savedGame = getGameFromLocalStorage();
     console.log('savedGame: ', savedGame);
 
-    if (savedGame) {
+    if (savedGame && savedGame.gameStatus !== "failed") {
         return {
             ...savedGame,
-            theme: theme,
-            level: level,
+            theme: savedGame.theme || theme,
+            level: savedGame.level || level,
             cards: savedGame.cards.map((card: Card) =>
                 card.isMatched ? card : { ...card, isFlipped: false }
             ),
             flippedCards: [],
         };
     }
+
     return {
         flippedCards: [],
         moves: 0,
@@ -28,3 +29,5 @@ export const initializeGame = (theme: Themes, level: Levels): GameState => {
         countDownTimer: getTimerByLevel(level)
     };
 };
+
+//fix the return when there is no game
