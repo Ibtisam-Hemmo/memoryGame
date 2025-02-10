7ee167a695948e31740145bb3aba317ae5893f41
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-10 18:18:45 +0200

    clean import &  export

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,13 +1,27 @@
-import apple from "./appleCharacter.jpeg";
-import banana from "./bananaCharacter.jpg";
-import grape from "./grapeCharacter.jpg";
 import strawberry from "./StrawberryCharacter.jpg";
-import orange from "./OrangeCharacter.jpg";
+import jerry from "./Jerry.jpg";
+import PapaSmurf from "./PapaSmurf.jpg";
+import Shrek from "./Shrek.jpg";
+import Tom from "./Tom.jpg";
+import duck from "./duck.jpg";
+import helloKitty from "./helloKitty.jpg";
+import mickeyMouse from "./mickeyMouse.jpg";
+import patrickStar from "./patrickStar.jpg";
+import popeye from "./popeye.jpg";
+import bunny from "./bunny.jpg";
+import scoobyDoo from "./scoobyDoo.jpg";
+import spongeBob from "./spongeBob.jpg";
+import tigger from "./tigger.jpg";
+import tweetyBird from "./tweetyBird.jpg";
+import winnieThePooh from "./winnieThePooh.jpg";
+import yogiBear from "./yogiBear.jpg";
+import mcCormick from "./mcCormick.jpg";
 
 export {
-    apple,
-    banana,
-    grape,
-    strawberry,
-    orange
-};
+    strawberry, yogiBear, winnieThePooh,
+    tweetyBird, tigger, spongeBob,
+    scoobyDoo, bunny, popeye,
+    patrickStar, mickeyMouse, helloKitty,
+    duck, Tom, Shrek,
+    PapaSmurf, jerry, mcCormick
+};  
