333d31b02d6dda0043d7c1b8a93581361739b9ca
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-11 16:32:00 +0200

    apply effect to cards

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -4,6 +4,7 @@ import { Card } from '../types/gameType';
 
 const GameBoard = () => {
   const { gameState, flipCard } = useGameContext();
+
   const renderCardContent = (card: Card) => {
     if (card.isFlipped || card.isMatched) {
       return card.type === "images" ? (
@@ -20,28 +21,32 @@ const GameBoard = () => {
   };
 
   return (
-    <>
-      <div className={styles.container}>
-        <div
-          className={styles.grid}
-          style={{
-            gridTemplateColumns: `repeat(${gameState.gridSize.columns}, minmax(60px, 1fr))`,
-            gridTemplateRows: `repeat(${gameState.gridSize.rows}, minmax(60px, 1fr))`,
-          }}
-        >
-          {gameState.cards.map((card) => (
+    <div className={styles.container}>
+      <div
+        className={styles.grid}
+        style={{
+          gridTemplateColumns: `repeat(${gameState.gridSize.columns}, minmax(60px, 1fr))`,
+          gridTemplateRows: `repeat(${gameState.gridSize.rows}, minmax(60px, 1fr))`,
+        }}
+      >
+        {gameState.cards.map((card, index) => {
+          const colIndex = index % gameState.gridSize.columns;
+          const delay = `${colIndex * 150}ms`;
+
+          return (
             <div
               key={card.id}
               onClick={() => !card.isMatched && !card.isFlipped && flipCard(card.id)}
               className={`${styles.cellFace} ${card.isFlipped || card.isMatched ? styles.flipped : ""}`}
+              style={{ animationDelay: delay }}
             >
               {renderCardContent(card)}
             </div>
-          ))}
-        </div>
-
+          );
+        })}
       </div>
-    </>)
-}
+    </div>
+  );
+};
 
-export default GameBoard
+export default GameBoard;
