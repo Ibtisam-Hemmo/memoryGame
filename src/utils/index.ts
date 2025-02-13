2cac0f9a3e244e59c5f5063661ee53eaad5882d6
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-13 11:53:45 +0200

    clean import & export

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export { gameLogic, checkForMatched } from "./gameLogic";
+export { generateCards, getTimerByLevel, getGridSize } from "./generateCards";
+export { initializeGame } from "./initializeGame";
+export { saveGameToLocalStorage, getGameFromLocalStorage } from "./localStorage";
+export { randomLostQuote, randomWinnerQuote } from "./quotes";
+export { shuffle } from "./shuffleFunction";
+export { sounds, playSound } from "./soundManager";
