777da817e8ffc989dd692eb1c1c4d46c34852854
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-16 16:32:59 +0200

    Fix Footer

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,27 +1,30 @@
 import { useNavigate } from 'react-router-dom'
-import { Footer, GameBoard, Header,GameInfo } from '../components'
+import { Footer, GameBoard, Header, GameInfo } from '../components'
 import { useGameContext } from '../context/gameContext'
 import styles from '../styles/GamePage.module.scss'
 
 const GamePage = () => {
     const { gameState: { countDownTimer, gameStatus } } = useGameContext();
     const navigate = useNavigate();
-    
+
     if (countDownTimer === 0 && gameStatus === "failed") {
         navigate("/game/failed");
-    } else if (gameStatus === "completed"){
+    } else if (gameStatus === "completed") {
         navigate("/game/completed")
     }
     return (
-        <main>
-            <Header />
-            <h2 className={styles.gameTitle}>Game Board</h2>
-            <div className={styles.gameContainer}>
-                <GameBoard />
-                <GameInfo />
-            </div>
+        <>
+            <main>
+                <Header />
+                <h2 className={styles.gameTitle}>Game Board</h2>
+                <div className={styles.gameContainer}>
+                    <GameBoard />
+                    <GameInfo />
+                </div>
+            </main>
             <Footer />
-        </main>
+        </>
+
     )
 }
 
