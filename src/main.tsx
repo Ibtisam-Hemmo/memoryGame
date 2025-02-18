cb46bdeed8f0bc3328278a67124d77a3dc1ce123
ibtisam-hemmo <ibtisamhemmo@gmail.com>
2025-02-18 12:12:54 +0200

    fix handling 404 pages in github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,29 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { GameProvider } from './context/gameContext.tsx'
-import { SettingsPage, GamePage, NotFoundPage } from './pages/index.ts'
+import { GameProvider } from './context/gameContext.tsx';
+import { SettingsPage, GamePage, NotFoundPage } from './pages/index.ts';
 import { GameOver, CompletedGame } from './components/index.ts';
-import "./styles/general.scss";
+import './styles/general.scss';
+
+const basePath = import.meta.env.DEV ? '/' : '/memoryGame/'; 
 
 createRoot(document.getElementById('root')!).render(
   <GameProvider>
-    <Router basename={import.meta.env.DEV ? '/' : '/memoryGame/'}>
-    <Routes>
+    <Router basename={basePath}>
+      <Routes>
         <Route path="/" element={<SettingsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
         <Route path="/game" element={<GamePage />} />
         <Route path="/game/failed" element={<GameOver />} />
         <Route path="/game/completed" element={<CompletedGame />} />
+        {import.meta.env.DEV ? (
+          // In development
+          <Route path="*" element={<NotFoundPage />} />
+        ) : (
+          // In production
+          <Route path="/notfound" element={<NotFoundPage />} />
+        )}
       </Routes>
     </Router>
-  </GameProvider>,
-)
-
+  </GameProvider>
+);
